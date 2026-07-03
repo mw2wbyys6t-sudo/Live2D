1ee8f3b413d015612714aa6006340b7eb5696154
mw2wbyys6t-sudo <mw2wbyys6t-sudo@users.noreply.github.com>
2026-07-03 04:03:44 +0000

    feat: 拉取代码仓库Main分支
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- comfyui-connector/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { ComfyUIConnector } from './connectors/comfyui.connector';
-export { ImageGenerationService } from './services/image-generation.service';
-export { FileManager } from './utils/file-manager';
-
-export * from './types';
